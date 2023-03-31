ab1cbd96491da81bb67b3033d96b56a3a385949b
Mikeykblond1 <mike.kralovic@outlook.com>
2023-03-31 16:42:31 -0700

    hi

---
+++ main.js
@@ -0,0 +1,10 @@
+var name1 = "Mike";
+var name2 = "Kralovic";
+var num1 = 20;
+var num2 = 5;
+
+console.log(name1.concat(' ',name2,' is an egg.'));
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
